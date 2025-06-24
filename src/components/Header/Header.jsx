c38d55ccb9c4f7f0ce6d0558b4eb03e6b45f396f
SD <jake2017tg@gmail.com>
2025-06-24 09:57:06 +0300

    -

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Logo from '../../assets/Logo.svg';
 const Header = () => {
   return (
     <header className="header">
-      <img src={Logo} alt="Aviasales Logo" />
+      <img src={Logo} alt="Aviasales Logo"/>
     </header>
   );
 };
